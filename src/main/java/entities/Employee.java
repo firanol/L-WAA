5ca0e2f81825476e83f34a6cb98f76c9296c1883
firanol <firanolm@gmail.com>
2024-10-03 07:34:01 -0500

    Initial commit for Lab3 project

---
+++ src/main/java/entities/Employee.java
@@ -0,0 +1,49 @@
+package com.example.associationdemo.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    // Constructors, Getters, Setters
+    public Employee() {}
+
+    public Employee(String name, Department department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
